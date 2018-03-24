a7064aec54c3a8d43aa5dcd3dcfed9495362cdbb
derHeinz <heinzserfas@gmx.de>
2018-03-24 10:52:42 +0100

    added fadein tests

--- alarmclock/player/player_test.py
+++ alarmclock/player/player_test.py
@@ -1,51 +1,116 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import unittest
 from player import Player
-from player_win import PlayerWin
 import logging
+import time
+from apscheduler.triggers.cron import CronTrigger
+from logging import StreamHandler
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 
 class PlayerTest(Player):
 	
 	def __init__(self, scheduler):
 		super(PlayerTest, self).__init__(scheduler)
 		self._volume = 0
+		self._stored_volumes = []
+		
+	def get_stored_volumes(self):
+		return self._stored_volumes
 		
 	def get_volume(self):
 		return self._volume
 		
 	def set_volume(self, value):
 		self._volume = value
-		if (self._volume > 100):
-			self._volume = 100
-		elif (self._volume < 0):
-			self._volume = 0
 		logging.debug("setting value to " + str(self._volume))
+		self._stored_volumes.append(value)
 			
 	def stop(self):
 		logging.debug("stopping player")
 
 	def _play(self):
 		logging.debug("playing player")
-
 		
-#from apscheduler.triggers.cron import CronTrigger
-#from logging.handlers import RotatingFileHandler
-#from apscheduler.schedulers.blocking import BlockingScheduler
-# setting up logging
-#formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-#handler = RotatingFileHandler('player_test.log', maxBytes=153600, backupCount=3)
-#handler.setFormatter(formatter)
+class PlayerTestCase(unittest.TestCase):
 
-#root_logger = logging.getLogger()
-#root_logger.setLevel(logging.DEBUG)
-#root_logger.addHandler(handler)
+	@classmethod
+	def setUpClass(cls):
+		# setting up logging
 
-#scheduler = BlockingScheduler()
-#p = PlayerWin(scheduler)
-#p.set_url("bell.wav")
-#p.set_volume(95)
-#p.play_fadein()
-#p.play()
-#scheduler.start() # blocking scheduler than this blocks!
+		# setting up the scheduler
+		cls._scheduler = BackgroundScheduler()
+		cls._scheduler.start()
+		
+	def tearDown(self):
+		PlayerTestCase._scheduler.remove_all_jobs()
+		
+	def create_new_player(self):
+		p = PlayerTest(PlayerTestCase._scheduler)
+		p.set_url("bell.wav")
+		return p
+		
+	def test_simple_schedule(self):
+		p = self.create_new_player()
+
+		# configure
+		p.set_volume(100)
+		p.set_fadein(True)
+		p.set_fadein_step_size(10)
+		p.set_fadein_steps(5)
+		p.set_fadein_interval(1)
+
+		# start
+		p.play()
+		
+		# wait until finished
+		time.sleep(10)
+		
+		# test player's values (initially set to 100, than the 5 steps with the size occure until it is set back to initial value
+		self.assertEquals(p.get_stored_volumes(), [100, 50, 60, 70, 80, 90 ,100])
+
+	def test_crazy_numbers(self):
+		p = self.create_new_player()
+
+		# configure
+		p.set_volume(93)
+		p.set_fadein(True)
+		p.set_fadein_step_size(7)
+		p.set_fadein_steps(11)
+		p.set_fadein_interval(1)
+
+		# start
+		p.play()
+		
+		# wait until finished
+		time.sleep(20)
+		
+		# test player's values
+		self.assertEquals(p.get_stored_volumes(), [93, 16, 23, 30, 37, 44, 51, 58, 65, 72, 79, 86, 93])
+		
+	def test_scheduler_still_runs(self):
+		p = self.create_new_player()
+
+		# configure
+		p.set_volume(50)
+		p.set_fadein(True)
+		p.set_fadein_step_size(5)
+		p.set_fadein_steps(2)
+		p.set_fadein_interval(10)
+
+		# start
+		p.play()
+		
+		# wait until finished
+		time.sleep(22)
+		
+		# test player's values
+		self.assertEquals(p.get_stored_volumes(), [50, 40, 45])
+		
+		
+
+if __name__ == '__main__':
+    unittest.main()
